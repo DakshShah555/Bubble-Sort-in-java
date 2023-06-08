83583ee91e9b4f560586666d8586e12d8a234063
Daksh Shah <135730010+DakshShah555@users.noreply.github.com>
2023-06-09 00:19:56 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,31 @@
+// time complexity of this coe is O(n^2)
+
+
+public class BubbleSort {
+    public static void bubbleSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5, 4, 1, 3, 2};
+        bubbleSort(arr);
+        printArr(arr);
+    }
+}
